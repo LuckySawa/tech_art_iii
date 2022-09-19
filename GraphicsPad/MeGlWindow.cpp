61fa209b0af0814185a8364474ecff9989b6556b
LuckySawa <ruizeg2019@gmail.com>
2022-09-18 18:42:34 -0600

    W4 - Draw a knife

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -7,11 +7,24 @@ void MeGlWindow::initializeGL()
 
 	GLfloat verts[] =
 	{
-		+0.0f, +0.0f,
+		/*+0.0f, +0.0f,
 		+1.0f, +1.0f,
 		-1.0f, +1.0f,
 		-1.0f, -1.0f,
-		+1.0f, -1.0f,
+		+1.0f, -1.0f,*/
+
+		-1.0f, +0.0f,
+		-0.8f, -0.2f,
+		+0.6f, +0.0f,
+		+0.6f, -0.2f,
+		+0.6f, +0.1f,
+		+0.6f, -0.3f,
+		+0.65f, +0.1f,
+		+0.65f, -0.3f,
+		+0.65f, +0.0f,
+		+0.65f, -0.2f,
+		+1.0f, +0.0f,
+		+1.0f, -0.2f,
 	};
 	GLuint myBufferID;
 	glGenBuffers(1, &myBufferID);
@@ -21,7 +34,7 @@ void MeGlWindow::initializeGL()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-	GLushort indices[] = { 3,1,2, 0,3,4 };
+	GLushort indices[] = { 0, 1, 2, 1, 2, 3, 4, 5, 6, 5, 6, 7, 8, 9, 10, 9, 10, 11 };
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
@@ -33,5 +46,5 @@ void MeGlWindow::paintGL()
 {
 	glViewport(0, 0, width(), height());
 	//glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, 0);
 }
