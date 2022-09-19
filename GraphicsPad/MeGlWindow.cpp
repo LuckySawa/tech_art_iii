12c842545c5ffe77ee00ea547985ae569cb259ef
LuckySawa <ruizeg2019@gmail.com>
2022-09-19 08:57:32 -0600

    Add some notes

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -1,6 +1,7 @@
 #include <gl\glew.h>
 #include <MeGlWindow.h>
 
+// Only run ONCE
 void MeGlWindow::initializeGL()
 {
 	glewInit();
@@ -27,14 +28,18 @@ void MeGlWindow::initializeGL()
 		+1.0f, -0.2f,
 	};
 	GLuint myBufferID;
+	// Create buffer and ref, bind, fill in data
 	glGenBuffers(1, &myBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 		verts, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+	// (index, size(xyzw), type, normalized, stride, pointer)
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	// Indices to form triangles
 	GLushort indices[] = { 0, 1, 2, 1, 2, 3, 4, 5, 6, 5, 6, 7, 8, 9, 10, 9, 10, 11 };
+	// Create buffer and ref, bind, fill in data
 	GLuint indexBufferID;
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
@@ -42,9 +47,15 @@ void MeGlWindow::initializeGL()
 		indices, GL_STATIC_DRAW);
 }
 
+// Update every frame
 void MeGlWindow::paintGL()
 {
+	// Draw based on screen size
 	glViewport(0, 0, width(), height());
+
+	// Follow the vertex sequence to draw triangles (might have two same vertices)
 	//glDrawArrays(GL_TRIANGLES, 0, 6);
+	
+	// Use the indices to draw triangles (no same vertices)
 	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, 0);
 }
